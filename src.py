313bb6af6ca9d4ec02751703548a026881bb350b
JonatasHdS <61765878+JonatasHdS@users.noreply.github.com>
2022-11-21 22:57:12 -0300

    Inicial

---
+++ src.py
@@ -0,0 +1,57 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def checkid(choice):
+    if choice in ('1', '2', '3', '4'):
+        return 1
+    else:
+        return 0
+
+def continuecalc(respost):
+    if respost == "nao":
+        return 0
+    elif respost == "sim":
+        return 1
+    else:
+        return -1
+
+print("Selecione uma operacao.")
+print("1.Soma")
+print("2.Subtração")
+print("3.Multiplicação")
+print("4.Dividir")
+
+while True:
+    choice = input("Escolha entre (1/2/3/4): ")
+
+    if checkid(choice) == 1:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(add(num1, num2))
+
+        elif choice == '2':
+            print(subtract(num1, num2))
+
+        elif choice == '3':
+            print(multiply(num1, num2))
+
+        elif choice == '4':
+            print(divide(num1, num2))
+        
+        next_calculation = input("Continuar? (sim/nao): ")
+        if continuecalc(next_calculation) == 0 or continuecalc(next_calculation) == -1:
+          break
+    
+    else:
+        print("Id nao reconhecido")
---
+++ tests/tests_calc.py
@@ -0,0 +1,34 @@
+def test_soma:
+    assert add(1,5) == 6, "Deveria ser 6"
+    assert add(5,8) == 13, "Deveria ser 13"
+    assert add(10,7) == 17, "Deveria ser 17"
+    assert add(20,54) == 74, "Deveria ser 74"
+
+def test_sub:
+    assert subtract(12,7) == 5, "Deveria ser 5"
+    assert subtract(17,10) == 7, "Deveria ser 7"
+    assert subtract(50,21) == 39, "Deveria ser 39"
+    assert subtract(31,2) == 29, "Deveria ser 29"
+
+def test_mult:
+    assert multiply(1,5) == 5, "Deveria ser 5"
+    assert multiply(14,2) == 28, "Deveria ser 28"
+    assert multiply(10,7) == 70, "Deveria ser 70"
+    assert multiply(20,4) == 100, "Deveria ser 100"
+
+def test_div:
+    assert add(18,2) == 9, "Deveria ser 9"
+    assert add(12,3) == 4, "Deveria ser 4"
+    assert add(50,5) == 10, "Deveria ser 10"
+    assert add(60,3) == 20, "Deveria ser 20" 
+
+def test_checkid:
+    assert checkid('1') == 1, "Deveria ser 1"
+    assert checkid('2') == 1, "Deveria ser 1"
+    assert checkid('3') == 1, "Deveria ser 1"
+    assert checkid('10') == 0, "Deveria ser 0"
+
+def test_continuecalc:
+    assert continuecalc("sim") == 1, "Deveria ser 1"
+    assert continuecalc("nao") == 0, "Deveria ser 0"
+    assert continuecalc("daada") == -1, "Deveria ser -1"
