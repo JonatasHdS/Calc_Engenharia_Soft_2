789c6d62c96e5273c872d6efebc7bf8d2617831f
JonatasHdS <61765878+JonatasHdS@users.noreply.github.com>
2022-11-21 23:35:54 -0300

    rework

--- tests/tests_calc.py
+++ tests/tests_calc.py
@@ -9,20 +9,14 @@ def test_soma():
 def test_sub():
     assert src.subtract(12,7) == 5, "Deveria ser 5"
     assert src.subtract(17,10) == 7, "Deveria ser 7"
-    assert src.subtract(50,21) == 39, "Deveria ser 39"
+    assert src.subtract(50,21) == 29, "Deveria ser 39"
     assert src.subtract(31,2) == 29, "Deveria ser 29"
 
 def test_mult():
     assert src.multiply(1,5) == 5, "Deveria ser 5"
     assert src.multiply(14,2) == 28, "Deveria ser 28"
     assert src.multiply(10,7) == 70, "Deveria ser 70"
-    assert src.multiply(20,4) == 100, "Deveria ser 100"
-
-def test_div():
-    assert src.add(18,2) == 9, "Deveria ser 9"
-    assert src.add(12,3) == 4, "Deveria ser 4"
-    assert src.add(50,5) == 10, "Deveria ser 10"
-    assert src.add(60,3) == 20, "Deveria ser 20" 
+    assert src.multiply(20,4) == 80, "Deveria ser 80"
 
 def test_checkid():
     assert src.checkid('1') == 1, "Deveria ser 1"
